857e010ff94257c2baca149798e32c523037ad18
Lauri Korts-Pärn <lauri@korc.jp>
2022-10-26 00:06:05 +0900

    update to focal binary release

--- usr/src/sfs.d/05-prepare-env.sh
+++ usr/src/sfs.d/05-prepare-env.sh
@@ -3,16 +3,9 @@
 set -e
 . "$(dirname "$0")/.common.sh"
 
-mkdir -p "$(dirname "$dpkg_status_save")"
-cp /var/lib/dpkg/status "$dpkg_status_save"
+mkdir -p "/usr/share/keyrings"
+echo "$RIA_KEY" | gpg --dearmor >"$ria_apt_key"
 
-mkdir -p "$deb_build_dir"
+echo "deb [signed-by=$ria_apt_key] $ria_repo $inst_dist main" >"$apt_list"
 
-echo "deb [trusted=yes] file://$deb_build_dir ./" > /etc/apt/sources.list.d/lbu-deb.list
-echo "deb-src [trusted=yes] file://$deb_build_dir ./" >> /etc/apt/sources.list.d/lbu-deb.list
-(cd "$deb_build_dir"; dpkg-scanpackages -m . >Packages; dpkg-scansources . >Sources)
-
-for x in Packages Sources; do
-  lists_fname="$(echo "$deb_build_dir/./$x" | tr / _)"
-  test -L "/var/lib/apt/lists/$lists_fname" || ln -vfs "$deb_build_dir"/$x "/var/lib/apt/lists/$lists_fname"
-done
+apt-get update
